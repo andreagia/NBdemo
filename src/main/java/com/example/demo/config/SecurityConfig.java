116df319bf797a68c3be4488dc4a6b10bef48671
andrea <andreagia71@gmail.com>
2019-02-14 10:41:05 +0100

    HTTP basic

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.example.demo.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        // rimane deprecato perche non e' consigliaìbile usare password in chiaro degli utenti
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,16 +1,21 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class IndexController {
 
     @GetMapping
     public String sayHello() {
-        return "Ciao sei nella pagina index di NBdemo il servizio REST è su " +
-                "/api/notes invia un richiesta POST su /api/notes per aggiungere un campo";
+        return "index";
+    }
+    @GetMapping({"/hellosec"})
+    public String greeting( Model model) {
+        model.addAttribute("name", "SPRING Security");
+        return "hello";
     }
 }
