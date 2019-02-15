7992e5752fb266a8da02e65b580cce3d5cdd8d2a
andrea <andreagia71@gmail.com>
2019-02-15 12:42:04 +0100

    Aggiunto SSL

--- src/main/java/com/example/demo/repository/NoteRepository.java
+++ src/main/java/com/example/demo/repository/NoteRepository.java
@@ -7,4 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
 
+//    @Query("SELECT con FROM Contact con  WHERE con.phoneType=(:pType) AND con.lastName= (:lName)")
+//    List<Contact> findByLastNameAndPhoneType(@Param("pType") PhoneType pType, @Param("lName") String lName);
+
 }
