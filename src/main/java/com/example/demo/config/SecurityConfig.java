64f2198842fa0e2a03a43ee5b0fca2b384ab67ef
andrea <andreagia71@gmail.com>
2019-02-16 12:07:39 +0100

    Test CSRF

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/","/api/bcrypt").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
--- src/main/java/com/example/demo/controller/NoteController.java
+++ src/main/java/com/example/demo/controller/NoteController.java
@@ -2,9 +2,11 @@ package com.example.demo.controller;
 
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Note;
+import com.example.demo.model.RestBCrypt;
 import com.example.demo.repository.NoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -16,6 +18,9 @@ public class NoteController {
 
     @Autowired
     NoteRepository noteRepository;
+
+    @Autowired
+    BCryptPasswordEncoder passwordEncoder;
 
     @GetMapping("/notes")
     public List<Note> getAllNotes() {
@@ -55,5 +60,11 @@ public class NoteController {
         noteRepository.delete(note);
 
         return ResponseEntity.ok().build();
+    }
+
+    // https://localhost:8443/api/bcrypt?passwd=pippo
+    @RequestMapping("/bcrypt")
+    public RestBCrypt bCrypt(@RequestParam(value="passwd", defaultValue="test") String passwd) {
+        return new RestBCrypt(passwd,passwordEncoder.encode(passwd));
     }
 }
